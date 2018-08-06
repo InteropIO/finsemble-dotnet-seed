bc612e8249ca6cb4ea2a71c52ecedc5a0a468034
Kris West <kris@chartiq.com>
2018-08-06 15:35:12 +0100

    Adding Windowless RPC example to seed project

---
+++ WindowlessExample/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WindowlessExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //Debugger.Launch(); // uncomment to launch debugger when executing externally.
+            var mainWindow = new MyClass(e.Args); // send command line arguments to main window.
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An Unhandled Exception has occured. Please Check your event Logs.");
+        }
+    }
+}
---
+++ WindowlessExample/MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChartIQ.Finsemble;
+using Newtonsoft.Json.Linq;
+
+namespace WindowlessExample
+{
+    internal class MyClass
+    {
+        private Finsemble FSBL;
+        public MyClass (string[] args)
+        {
+            FSBL = new Finsemble(args, null);
+            FSBL.Connect();
+            FSBL.Connected += FSBL_Connected;
+        }
+
+        private void FSBL_Connected(object sender, EventArgs e)
+        {
+            FSBL.RPC("Logger.log", new List<JToken> {
+                "Log Test"
+            });
+
+            
+        }
+    }
+}
