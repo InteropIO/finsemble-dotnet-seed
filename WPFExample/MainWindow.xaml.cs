40b918a15cf8c892a388ed57c199d3eace7def2d
Kris West <kris@chartiq.com>
2018-08-06 18:05:23 +0100

    adding styling settings for finsemble 3

--- WPFExample/MainWindow.xaml.cs
+++ WPFExample/MainWindow.xaml.cs
@@ -76,17 +76,16 @@ namespace WPFExample
                 });
 
                 //Styling the Finsemble Header
-                /* 
-                FinsembleHeader.SetActiveBackground(new SolidColorBrush(Colors.Red));
-                FinsembleHeader.SetInactiveBackground(new SolidColorBrush(Colors.DarkRed));
-                FinsembleHeader.SetButtonHoverBackground(new SolidColorBrush(Colors.Purple));
-                FinsembleHeader.SetInactiveButtonHoverBackground(new SolidColorBrush(Colors.Yellow));
-                FinsembleHeader.SetCloseButtonHoverBackground(new SolidColorBrush(Colors.SeaShell));
-                FinsembleHeader.SetInactiveCloseButtonHoverBackground(new SolidColorBrush(Colors.BurlyWood));
-                FinsembleHeader.SetDockingButtonDockedBackground(new SolidColorBrush(Colors.BlanchedAlmond));
-                FinsembleHeader.SetTitleForeground(new SolidColorBrush(Colors.LightGoldenrodYellow));
-                FinsembleHeader.SetButtonForeground(new SolidColorBrush(Colors.LightSalmon));
-                */
+                FinsembleHeader.SetActiveBackground(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3C4C58")));
+                FinsembleHeader.SetInactiveBackground(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#303D47")));
+                FinsembleHeader.SetButtonHoverBackground(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#005BC5")));
+                FinsembleHeader.SetInactiveButtonHoverBackground(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#004BA3")));
+                FinsembleHeader.SetCloseButtonHoverBackground(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#D30E2D")));
+                FinsembleHeader.SetInactiveCloseButtonHoverBackground(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#D30E2D")));
+                FinsembleHeader.SetDockingButtonDockedBackground(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#005BC5")));
+                FinsembleHeader.SetTitleForeground(new SolidColorBrush(Colors.White));
+                FinsembleHeader.SetButtonForeground(new SolidColorBrush(Colors.White));
+
                 FSBL.DragAndDropClient.SetScrim(Scrim); // The Scrim Label Control is used for drag and drop.
 
                 // Receivers for dropped data.
