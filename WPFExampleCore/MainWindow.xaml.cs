5ae7cf85e8a0238aab40bd5c1880e75cec43b5d0
kkirkeide <kyle@chartiq.com>
2022-07-25 18:13:47 -0400

    7.3 updates

--- WPFExampleCore/MainWindow.xaml.cs
+++ WPFExampleCore/MainWindow.xaml.cs
@@ -45,7 +45,8 @@ namespace WPFExampleCore
 			FSBL = new FinsembleWPF(this, handle.ToString("X"), args); // Finsemble needs the command line arguments to connect and also this Window handle to manage snapping, docking etc.
 			FSBL.Connected += Finsemble_Connected;
 			FSBL.Disconnected += FSBL_Disconnected;
-			var connectTask = FSBL.Connect(FSBL.AppName ?? "WPFExampleCore", JWK);
+			// For authentication in the InteropService use the appId from appd.json and static auth token
+			var connectTask = FSBL.Connect("Finsemble WPF Demo Core", JWK);
 		}
 
 		private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
